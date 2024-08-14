dbb4fc630e7253f3494d07cd0ad1a583b6e2ea75
dylnyko <dylan.nykolyszyn@gmail.com>
2024-08-14 16:57:03 +0100

    Update router and page title

--- src/router/router.js
+++ src/router/router.js
@@ -1,27 +1,27 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHistory } from 'vue-router'
 
 const routes = [
-    {
-        path: "/",
-        meta: { title: "Home" },
-        component: () => import("../views/HomePage.vue"),
-    },
-    {
-        path: "/:pathMatch(.*)*",
-        meta: { title: "404" },
-        component: () => import("../views/errors/404Page.vue"),
-    },
-];
+  {
+    path: '/',
+    meta: { title: 'Home' },
+    component: () => import('../views/HomePage.vue')
+  },
+  {
+    path: '/:pathMatch(.*)*',
+    meta: { title: '404' },
+    component: () => import('../views/errors/404Page.vue')
+  }
+]
 
 const router = createRouter({
-    history: createWebHistory(),
-    routes,
-});
+  history: createWebHistory(),
+  routes
+})
 
 // Set page title
 router.beforeEach(async (to, from, next) => {
-    document.title = `${to.meta.title} | ${import.meta.env.VITE_APP_NAME}`;
-    next();
-});
+  document.title = `${to.meta.title} | dylnyko`
+  next()
+})
 
-export default router;
+export default router
